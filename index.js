2a7b6d3fd527193bf0af32002a04a9f3355e9854
Yoann Perro <yoann.perro@gmail.com>
2016-11-14 15:31:26 +0100

    Set port to process.env.port

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 var express = require('express');
+var bodyParser = require('body-parser')
+
+// Setup Express
 var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// POST method route
+app.post('/team', function (req, res) {
+  res.send('POST request on /team');
+});
 
 // GET method route
 app.get('/', function (req, res) {
-  res.send('GET request to the homepage');
-});
-
-// POST method route
-app.post('/', function (req, res) {
-  res.send('POST request to the homepage');
+  res.send('GET request on /');
 });
 
 // Start server
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port);
 });
