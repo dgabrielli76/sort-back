0dc54e8d5bbd6c93f9abacd67018adda88891a3b
Yoann Perro <yoann.perro@gmail.com>
2016-11-14 15:03:39 +0100

    Add Node.js Express skeleton

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+// GET method route
+app.get('/', function (req, res) {
+  res.send('GET request to the homepage');
+});
+
+// POST method route
+app.post('/', function (req, res) {
+  res.send('POST request to the homepage');
+});
+
+// Start server
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
